e52657ea1a5d98da295ba9ae388bd08a805cb626
dhruv_signalx_ai <dhruv@signalx.ai>
2021-08-09 02:17:34 +0530

    Added magnum package and mentioned in requirements.txt and also defined AWS lambda deployment CD pipeline

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from mangum import Mangum
 
 app = FastAPI()
 
@@ -7,6 +8,8 @@ app = FastAPI()
 async def root():
     return {"message":"Hello from AWS Lambda!"}
 
+
+handler = Mangum(app=app)
 
 # if __name__ == "__main__":
 #     uvicorn.run(app, host="0.0.0.0", port=8000)
